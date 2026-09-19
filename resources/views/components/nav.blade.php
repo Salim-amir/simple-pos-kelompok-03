2cbf3be6047b09669c3cd98dd601e7607baa4591
R. Rawdhoh Ardhillah <rrawdhohardhillah@gmail.com>
2026-09-19 23:10:10 +0700

    rapikan tampilan nav

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,9 @@
 <nav class="bg-slate-900 text-white px-4 py-3 flex gap-4">
+<div class="flex items-center gap-4">
 <span class="font-semibold">Simple POS</span>
+</div>
+<div class="flex gap-4"> 
 <a href="{{ route('pos.create') }}" class="hover:underline">Kasir</a>
 <a href="{{ route('transactions.index') }}" class="hover:underline">Transaksi</a>
+</div>
 </nav>
